2435481f8b6957be618ccff2e9b107a182efe67f
Albert <alertu@bk.ru>
2026-04-25 18:47:50 +0800

    Update Rational.h

--- Rational/Rational.h
+++ Rational/Rational.h
@@ -5,37 +5,47 @@ using namespace std;
 
 class Rational {
 private:
-    int numer;
-    int denom;
-
-    void simplify();
+    // числитель
+    int numer; 
+    // знаменатель
+    int denom; 
+    // функция упрощения
+    void simplify(); 
 
 public:
-    // Конструкторы
-    Rational();
-    Rational(int number);
-    Rational(int n, int d);
+    // конструктор по умолчанию
+    Rational();       
+    // конструктор из целого (преобразующий)
+    Rational(int number); 
+    // конструктор с двумя аргументами
+    Rational(int n, int d);           
 
-    // Арифметические операции
-    Rational& operator +=(const Rational& r);
-    Rational operator +(const Rational& r) const;
-    Rational operator -() const;
+    // арифметические операции
+    Rational& operator =(const Rational& r);
+    Rational& operator +=(const Rational& r); 
+    Rational operator +(const Rational& r) const; 
+    Rational operator -() const;   
     Rational operator -(const Rational& r) const;
-    Rational& operator -=(const Rational& r);
+    Rational& operator -=(const Rational& r); 
+    Rational& operator *=(const Rational& r);
 
-    // Инкремент
-    Rational& operator ++();
-    Rational operator ++(int);
+    // операторы инкремента
+    Rational& operator ++(); // префикс
+    Rational operator ++(int); // постфикс
 
-    // Сравнение
-    bool operator ==(const Rational& r) const;
-    bool operator !=(const Rational& r) const;
+    // операторы сравнения
+    bool operator ==(const Rational& r) const; 
+    bool operator !=(const Rational& r) const; 
+    bool operator >(const Rational& r) const;
+    bool operator <(const Rational& r) const;
+    bool operator >=(const Rational& r) const;
+    bool operator <=(const Rational& r) const;
 
     // Преобразование типов
-    operator int() const;
+    operator int() const;  
     operator double() const;
 
     // Ввод-вывод 
-    friend istream& operator >>(istream& in, Rational& r);
-    friend ostream& operator <<(ostream& out, const Rational& r);
+    friend istream& operator >>(istream& in, Rational& r);     
+    friend ostream& operator <<(ostream& out, const Rational& r); 
 };
